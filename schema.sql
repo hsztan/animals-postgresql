241381d73f725248c2d0545e83b459fc16766cf2
Henry Nawrocki <hdev@sztanski.com>
2022-05-30 15:21:45 -0500

    Insert values to animals table

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id integer,
+    id int PRIMARY KEY,
     name varchar(100),
     date_of_birth date,
     escape_attempts integer,
