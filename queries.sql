68d211222155894c7e958fda7a366b8fd3347bf2
Henry Nawrocki <hdev@sztanski.com>
2022-05-31 11:08:38 -0500

    Add queries for assigned questions

--- queries.sql
+++ queries.sql
@@ -8,3 +8,19 @@ SELECT (name, escape_attempts) FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered FROM animals
+WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
+
+SELECT MAX(weight_kg), MIN(weight_kg), species FROM animals
+GROUP BY species;
+
+SELECT AVG(escape_attempts), species FROM animals
+WHERE date_of_birth > '1990-01-01' AND date_of_birth < '2000-01-01'
+GROUP BY species;
